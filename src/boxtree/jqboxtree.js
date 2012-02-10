6cce628a0ed23ddc084b72bdbcd330dd8ce8582d
Ken Pu <kenpuca@gmail.com>
2012-02-10 01:02:34 -0500

    improved the api

--- src/boxtree/jqboxtree.js
+++ src/boxtree/jqboxtree.js
@@ -5,29 +5,30 @@
   var clear = function() { 
     return $('<div>').css('clear', 'both');
   };
-  var mk_node = function(root) {
+  var mk_node = function(root, html) {
     var node = $('<div>').addClass("boxtree-node");
     if(root) node.addClass("boxtree-root");
-    $('<div>').addClass("boxtree-content").appendTo(node);
+    var c = $('<div>').addClass("boxtree-content").appendTo(node);
+    if(html != null) c.html(html);
     $('<div>').addClass("boxtree-children").appendTo(node);
     clear().appendTo(node);
     return node;
   };
-  var mk_child = function() {
+  var mk_child = function(html) {
     var child = $('<div>').addClass("boxtree-child");
     child.append(
       $('<div>').addClass("boxtree-connector"),
-      mk_node(),
+      mk_node(false, html),
       clear());
     return child;
   };
-  var node_content = function(node) {
+  var get_content = function(node) {
     return $(".boxtree-content:first", node);
   };
-  var node_children = function(node) {
+  var get_children = function(node) {
     return $(".boxtree-children:first", node);
   };
-  var node_connector = function(node) {
+  var get_connector = function(node) {
     if(node.hasClass("boxtree-node"))
       return node.parent().children(".boxtree-connector");
     else if(node.hasClass("boxtree-child"))
@@ -36,11 +37,11 @@
       throw "node is not node or child: " + node.attr('class');
   };
   var connect_children = function(node) {
-    var children = node_children(node).children();
+    var children = get_children(node).children();
     var n = children.size();
-    var h0 = node_content(node).height()/2;
+    var h0 = get_content(node).height()/2;
     children.each(function(i, child) {
-          var conn = node_connector($(child));
+          var conn = get_connector($(child));
           var h = $(child).outerHeight(1);
           var h1 = h-h0;
           var d1 = $('<div>').addClass("boxtree-c1").css('height', h0);
@@ -50,32 +51,84 @@
             $.each([d1, d2, d3], function(i, e) {
                 e.addClass("boxtree-first");
               });
-          } else if (i == n-1) {
+          } 
+          if (i == n-1) {
             $.each([d1, d2, d3], function(i, e) {
                 e.addClass("boxtree-last");
               });
           }
-          conn.append(d1, d2, clear(), d3, clear());
+          conn.empty().append(d1, d2, clear(), d3, clear());
         });
+  };
+
+  var get_parent = function(node) {
+    return node.closest(".boxtree-node");
+  };
+
+  var traverse = function(node, f) {
+    var content = get_content(node);
+    var children = get_children(node).children();
+    var parent = get_parent(node);
+    var ret = f({node: node, 
+                 content: content, parent: parent, children: children});
+    if(ret == false)
+      return;
+    children.each(function(i, ch) { traverse($(ch), f); });
+  };
+
+  /*
+   * We expect a structure from x0 as
+   * <ul>
+   *   <li><span> text here </span>
+   *     <ul> ... </ul>
+   *   </li>
+   *   ...
+   * </ul>
+   */
+
+  var mk_tree = function(x0, y0) {
+    $(x0).children("li").each(function(i, li) {
+      var span = $(li).children("span");
+      var ul = $(li).children("ul");
+      var y = y0.boxtree('add-child', span.html());
+      if(ul.size() > 0) mk_tree(ul, y);
+    });
   };
 
   var methods = {
     'init' : function(o) {
-      var n = mk_node(true);
-      this.empty().append(n);
-      return n;
+      o = $.extend({
+            root: "Root"
+          }, o);
+      var root = mk_node(true, o.root);
+      var ul = (this.is("ul")) ? this : $("ul:first", this);
+      if(ul.size() > 0) {
+        mk_tree(ul, root);
+        ul.replaceWith(root);
+      } else {
+        this.append(root);
+      }
+      root.boxtree('connect', {recursive:1});
+      return root;
     },
-    'add-child' : function(o) {
-      var n = mk_child();
-      var children = node_children(this);
-      node_children(this).append(n);
+    'add-child' : function(html) {
+      var n = mk_child(html);
+      var children = get_children(this);
+      get_children(this).append(n);
       return n;
     },
     'set-content': function(o) {
-      node_content(this).empty().append(o);
+      get_content(this).empty().append(o);
     },
     'connect': function(o) {
-      connect_children(this);
+      if(o.recursive) {
+        this.boxtree('traverse', function(x) {connect_children(x.node);});
+      } else
+        connect_children(this);
+    },
+    'traverse': function(f) {
+      traverse(this, f);
+      return this;
     }
   };
 
