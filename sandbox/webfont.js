3274e056ecd5783c65621dd0ade15cf767436804
Ken Pu <kenpuca@gmail.com>
2012-02-22 00:37:03 -0500

    two way window message passing

---
+++ sandbox/webfont.js
@@ -0,0 +1,7 @@
+$(document).ready(function() {
+    var $link = $('<link>').attr({
+        href: 'http://fonts.googleapis.com/css?family=Ubuntu+Mono',
+        rel: 'stylesheet',
+        type: 'text/css'
+      }).appendTo($('head'));
+    });
