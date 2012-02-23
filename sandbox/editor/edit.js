3ea3239829b0e7011d48c0c9f5022faa7d22fa80
100324932 <kenpu@ubuntu.ubuntu-domain>
2012-02-23 01:36:34 -0500

    editor updated

--- sandbox/editor/edit.js
+++ sandbox/editor/edit.js
@@ -1,11 +1,11 @@
 function add_control($node) {
   var $content = $node.find(".content:first");
   var $control = _div_(".control").append(
-        _span_(".add", '+'),
-        _span_(".remove", '-'),
+        _span_(".add", 'Add'),
+        _span_(".remove", 'Remove'),
         _span_(".open", 'Close'),
-        _span_(".edit", 'edit'),
-        _span_(".author", 'author')
+        _span_(".edit", 'Edit'),
+        _span_(".author", 'Author')
       );
   $content.append($control);
   return $control;
@@ -22,20 +22,17 @@ function get_control($node) {
   return $(".control", get_content($node));
 }
 
-function set_content($n, name, block_content) {
-  var $cont = get_content($n);
-  var $block = $cont.children(".block");
-  var $name, $desc;
-  if($block.size() > 0) {
-    $name = $(".name", $block);
-    $desc = $(".description", $block);
-  } else {
-    $name = _div_(".name");
-    $desc = _div_(".description");
-    $cont.prepend(_div_(".block").append($name, $desc));
-  }
-  $name.html(name);
-  $desc.empty().append(block_content);
+function set_content($n, name, desc) {
+  var $content = get_content($n);
+  var $name = _div_(".name").append(name);
+  var $desc = _div_(".description").empty().append(desc);
+  var $name_ed = _div_(".name-edit").hide()
+                 .append($("<input>"));
+  var $desc_ed = _div_(".description-edit").hide()
+                 .append($("<textarea>"));
+  $content.empty().append(
+    _div_('.block').append($name, $desc, $name_ed, $desc_ed)
+  );
   add_control($n).hide();
   return $n;
 }
@@ -88,7 +85,16 @@ function set_callbacks($root) {
     ev.stopPropagation();
     ev.preventDefault();
   });
-
+  $root.on('click', '.control .edit', function(ev) {
+    var $n = $(this).closest('.node');
+    edit_node($n);
+    ev.stopPropagation();
+    ev.preventDefault();
+  });
+  $root.on('click', '.name-edit,.description-edit', function(ev) {
+    ev.stopPropagation();
+    ev.preventDefault();
+  });
 }
 
 function activate_node($n) {
@@ -101,7 +107,35 @@ function deactivate_node($n) {
       deactivate_node($(e));
     });
   } else {
+    save_node($n);
     $n.removeClass("activate");
     get_control($n).fadeOut();
+  }
+}
+
+function edit_node($n) {
+  $n.addClass("edited");
+  var $content = get_content($n);
+  var $n = $content.find(".name").hide();
+  var $d = $content.find(".description").hide();
+  var $ne = $content.find(".name-edit").show();
+  var $de = $content.find(".description-edit").show();
+  $ne.find("input").val($n.text());
+  $de.find("textarea").val($d.html());
+}
+
+function save_node($n) {
+  if($n.hasClass("edited")) {
+    $n.removeClass("edited");
+    var $content = get_content($n);
+    var $ne = $content.find(".name-edit").hide();
+    var $de = $content.find(".description-edit").hide();
+    var $n = $content.find(".name").show();
+    var $d = $content.find(".description").show();
+    $n.text($ne.find("input").val());
+    $d.html($de.find("textarea").val());
+    if(MathJax != null) {
+      MathJax.Hub.Queue(['Typeset', MathJax.Hub, $d[0]]);
+    }
   }
 }
