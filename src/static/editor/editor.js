0925304119ab88fcb4e3b8cfc72e6534f8e8666c
Ken Pu <kenpuca@gmail.com>
2012-03-02 15:16:18 -0500

    editing name and desc

--- src/static/editor/editor.js
+++ src/static/editor/editor.js
@@ -10,18 +10,67 @@ var conceptTree = new ConceptTree([
     desc: "Complex structures and putting things back in order"
   },
   { name: "Code generation",
-    desc: "Finall, run Lola run"
+    desc: "Finally, run Lola run"
   },
 ]);
+
+(function($) {
+  $.fn.Edit = function() {
+    var $d = $("#node-edit-dialog");
+    var m = $(this).T_Model();
+    $d.data('node', $(this));
+    $d.find("input").val(m.get('name'));
+    $d.find("textarea").val(m.get('desc'));
+    $d.dialog('open');
+  };
+  $.fn.Save = function() {
+    var $d = $("#node-edit-dialog");
+    var m = $(this).T_Model();
+    m.set({
+      name: $d.find("input").val(),
+      desc: $d.find("textarea").val()
+    });
+    $(this).find(".name .display").text(m.get('name'));
+    $(this).find(".desc .display").html(m.get('desc'));
+    alert('new name = ' + m.get('name'));
+  };
+})(jQuery);
 
 $(function() {
 
   var container_template = _.template($("#container-template").html());
 
+  /* Events */
   $("#concept-tree")
-    .on("click", ".bullet", function(e) {
-      $(this).T_Node().T_ToggleCollapse();
+    .on("click", ".container", function(e) {
+      var $n = $(this).T_Node();
+      if($n.is(".focused"))
+        $n.removeClass("focused");
+      else {
+        $("#concept-tree .node").removeClass("focused");
+        $n.addClass("focused");
+      }
+      return false;
     })
+    .on("dblclick", ".container .main", function(e) {
+      $(this).T_Node().Edit();
+      return false;
+    })
+    .on("click", ".expand-collapse", function(e) {
+      var $n = $(this).T_Node();
+      $n.toggleClass("collapsed");
+      if($n.is(".collapsed")) {
+        $(this).text("Expand");
+        $n.find(".children:first").hide();
+      } else {
+        $(this).text("Collapse");
+        $n.find(".children:first").show();
+      }
+      return false;
+    })
+    ;
+
+  $("#concept-tree")
     .on("node-loaded", ".node", function(e, $n) {
       $n.find(".container:first")
         .html(container_template($n.T_Model().toJSON()));
